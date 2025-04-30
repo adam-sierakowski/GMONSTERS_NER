903f40f49dd4a4b566a549cd3a3b31961552c71b
adam-sierakowski <adsierak@gmail.com>
2025-04-30 20:39:26 +0200

    Remove unnecessary files

--- monsters/deduplicate_declension.py
+++
@@ -1,7 +0,0 @@
-with open("01_monsters_declined_gpt.txt", "r") as input_f, \
-open("02_monsters_declined_deduplicated.txt", "w") as output_f:
-    for line in input_f:
-        deduplicated = "|".join(sorted(list(set(line.strip().split(";"))))).replace("||", "|")
-        if deduplicated.startswith("|"):
-            deduplicated = deduplicated[1:]
-        output_f.write(deduplicated + "\n")
